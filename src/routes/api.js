2ac18549709db60b54a4a87c8d456159ea15fa54
JoiZs <pyaesoneaung1126@hotmail.com>
2023-11-13 12:37:29 -0500

    auth auth for each reqs in db_apis

--- src/routes/api.js
+++ src/routes/api.js
@@ -6,21 +6,22 @@ const {
   listUsers,
   updateUser,
 } = require("../controllers/user-controller");
+const checkAuth = require("../middleware/checkTk");
 
 // 5 apis
 // Create user/ Sign Up
 router.post("/users", createUser);
 
 // List all users from the database
-router.get("/users", listUsers);
+router.get("/users", checkAuth, listUsers);
 
 // GEttting one user from database using UserID
-router.get("/users/:userId", fetchUser);
+router.get("/users/:userId", checkAuth, fetchUser);
 
 // Updating one user from database using UserID
-router.put("/users/:userId", updateUser);
+router.put("/users/:userId", checkAuth, updateUser);
 
 // Deleting one user from database using UserID
-router.delete("/users/:userId", deleteUser);
+router.delete("/users/:userId", checkAuth, deleteUser);
 
 module.exports = router;
